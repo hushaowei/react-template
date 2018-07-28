102a116003af0241326ae7710581c8e6aec8058b
hushaowei <shaowei_hu@kingdee.com>
2018-07-28 13:37:55 +0800

    用webpack-dev-server构建生产环境

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,5 +5,9 @@ export default {
     path: path.resolve(__dirname, ''),
     filename: 'bundle.js'
   },
+  devServer: {
+    port: 8090, // 端口
+    open: true, // 运行时打开
+  },
   mode: 'development'
 }
