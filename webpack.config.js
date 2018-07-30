fb2ff239c43fc234002cb3589a4ee2170f3d1553
hushaowei <shaowei_hu@kingdee.com>
2018-07-30 19:36:05 +0800

    简单的react例子

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,16 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)/,
+        exclude: /node_modules/, // 除了这个文件夹外
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
   }
 };
