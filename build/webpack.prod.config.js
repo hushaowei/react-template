64b85a8cd0f0ac216cbd34c3cee16151b5da398d
hushaowei <shaowei_hu@kingdee.com>
2018-07-31 14:42:36 +0800

    生产环境的配置文件

---
+++ build/webpack.prod.config.js
@@ -0,0 +1,34 @@
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var cleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    // path是webpack打包后文件的储存路径 （必须是绝对路径）
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'js/[id].[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)/,
+        exclude: /node_modules/, // 除了这个文件夹外
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new cleanWebpackPlugin(['dist/*'], {
+      root: path.resolve(__dirname, '../')
+    }),
+    new HtmlWebpackPlugin({
+      title: '这是一个标题',
+      template: './public/index.html', // 模板
+      inject: true // 将js文件放到body标签的结尾 true或者body
+    })
+  ]
+};
