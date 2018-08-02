528cdf47eda5d50207fccb3fd0808fb20d7e5f3c
hushaowei <shaowei_hu@kingdee.com>
2018-08-02 17:37:49 +0800

    增加导入css、less的支持

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -27,6 +27,14 @@ module.exports = {
         test: /\.(js)/,
         exclude: /node_modules/,// 除了这个文件夹外
         use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
       }
     ]
   },
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {render} from 'react-dom'
+import './default.css'
+import './common.less'
 
 render(
   <h1>Hello, world!</h1>,
